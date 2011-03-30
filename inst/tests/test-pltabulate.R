18f0f63226578efc2794e0dd2700ae1c340dec95
rjbgoudie <rjbgoudie@users.noreply.github.com>
2011-03-30 15:00:31 +0100

    Add boilerplate text for license

--- inst/tests/test-pltabulate.R
+++ inst/tests/test-pltabulate.R
@@ -1,3 +1,13 @@
+# Part of the "parental" package, http://github.com/rjbgoudie/parental
+# 
+# This software is distributed under the GPL-3 license.  It is free,
+# open source, and has the attribution requirements (GPL Section 7) in
+#   http://github.com/rjbgoudie/parental
+# 
+# Note that it is required that attributions are retained with each function.
+#
+# Copyright 2008 Robert J. B. Goudie, University of Warwick
+
 context("pltabulate")
 
 test_that("pltabulate", {
--- src/pdag.h
+++ src/pdag.h
@@ -1,9 +1,12 @@
 /*
- *  pdag.h
- *  
- *
- *  Created by Robert Goudie on 14/08/2010.
- *  Copyright 2010 __MyCompanyName__. All rights reserved.
- *
+ * Part of the "parental" package, http://github.com/rjbgoudie/parental
+ * 
+ * This software is distributed under the GPL-3 license.  It is free,
+ * open source, and has the attribution requirements (GPL Section 7) in
+ *   http://github.com/rjbgoudie/parental
+ * 
+ * Note that it is required that attributions are retained with each function.
+ * 
+ * Copyright 2008 Robert J. B. Goudie, University of Warwick
  */
 
