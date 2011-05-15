0ad7b23345cd33de90ca1808537d99b3b6f3cb89
rjbgoudie <rjbgoudie@users.noreply.github.com>
2011-05-15 12:37:15 +0100

    Improve documentation of the utilities

--- R/utils.R
+++ R/utils.R
@@ -8,8 +8,12 @@
 #
 # Copyright 2008 Robert J. B. Goudie, University of Warwick
 
+#' Tests for whole numbers
+#' 
 #' Tests if x is a whole number, up to the tolerance specified by tol.
-#' Taken from an example on the help page for is.integer()
+#' Taken from an example on the help page for \code{\link{is.integer}}. 
+#' 
+#' Note that is.integer(x) does not test if x contains integer numbers! 
 #' 
 #' @param x A numeric vector of length 1, which the function check if it is 
 #'   whole
@@ -18,6 +22,10 @@
 #' @return A logical vector of length 1, taking the value TRUE is x is a 
 #'   whole number and false otherwise
 #' @export
+#' @seealso \code{\link{is.integer}}
+#' @examples
+#' is.wholenumber(2)
+#' is.wholenumber(2.1)
 is.wholenumber <- function(x, tol = .Machine$double.eps^0.5){
   stopifnot(length(x) == 1,
             isTRUE(is.numeric(x)),
@@ -32,7 +40,12 @@ is.wholenumber <- function(x, tol = .Machine$double.eps^0.5){
 #'
 #' @param mx A matrix
 #' @return A vector of off-diagonals
+#' @seealso \code{\link{diag}}, \code{\link{lower.tri}}, 
+#'   \code{\link{upper.tri}}
 #' @export
+#' @examples
+#' m <- matrix(1:9, 3, 3)
+#' notdiag(m)
 notdiag <- function(mx){
   mx[which(upper.tri(mx) + lower.tri(mx) == 1)]
 }
