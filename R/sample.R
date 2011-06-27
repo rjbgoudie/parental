13da014760da54e965675ce167e5d8b73486ab83
rjbgoudie <rjbgoudie@users.noreply.github.com>
2011-06-27 08:59:18 +0100

    Correct code style

--- R/sample.R
+++ R/sample.R
@@ -24,24 +24,24 @@
 #' sampleBN(10, 2)
 sampleBN <- function(n, maxNumberParents = NULL){
   nodeSeq <- seq_len(n)
-  
+
   # sample an order
   ordering <- sample(nodeSeq, replace = F)
-  
+
   out <- lapply(nodeSeq, function(i){
     rank <- which(ordering == i)
     if (rank > 1){
       potentialParents <- ordering[seq_len(rank - 1)]
       numberOfPotentialParents <- length(potentialParents)
-      
-      if (!is.null(maxNumberParents) && 
+
+      if (!is.null(maxNumberParents) &&
           numberOfPotentialParents > maxNumberParents){
         numberOfPotentialParents <- maxNumberParents
       }
-      
+
       numberOfParents <- sample.int(numberOfPotentialParents + 1, size = 1)
       numberOfParents <- numberOfParents - 1
-      
+
       if (numberOfPotentialParents > 1){
         out <- sort.int(sample(potentialParents,
                                size = numberOfParents,
@@ -79,13 +79,13 @@ sampleBN <- function(n, maxNumberParents = NULL){
 sampleBN2 <- function(n, k){
   # hypercube
   # the higher k the sparser the partial order
-  
+
   x <- vapply(seq_len(n), function(x) runif(k), numeric(k))
   out <- empty(n, class = "bn")
   for (i in seq_len(n)){
     for (j in setdiff(seq_len(n), i)){
       # an arrow i -> j  if for all k, i < j
-      
+
       if (k == 1){
         if (all(x[i] < x[j])){
           out[[j]] <- sort.int(c(out[[j]], i))
@@ -174,25 +174,25 @@ simulate.bn <- function(object, nsim, seed, ptables, expectation = F, ...){
   else {
     creator <- sample.int
   }
-  
+
   nNodes <- length(object)
   numberOfParents <- sapply(object, length)
   dat <- as.data.frame(matrix(NA, ncol = nNodes, nrow = nsim))
-  
+
   # topologically sort
   nodeOrder <- topologicallyOrder(object)
-  
+
   numberOfLevelsAll <- lapply(seq_len(nNodes), function(i){
     dim(ptables[[i]])[1]
   })
-  
+
   # loop through the nodes in order
   for (i in nodeOrder){
     ptable <- ptables[[i]]
-    
+
     # first dimension is the values
     numberOfLevels <- numberOfLevelsAll[[i]]
-    
+
     if (numberOfParents[i] == 0){
       dat[, i] <- creator(numberOfLevels,
                              size    = nsim,
@@ -201,35 +201,35 @@ simulate.bn <- function(object, nsim, seed, ptables, expectation = F, ...){
     }
     else {
       parents <- object[[i]]
-      
+
       local <- dat[, parents, drop = F]
       local <- data.frame(lapply(seq_len(ncol(local)), function(i){
         ls <- seq_len(numberOfLevelsAll[[parents[i]]])
         factor(local[, i], levels = ls)
       }))
-      
+
       # find all the configurations of the parents that exist
       tab <- table(local)
       whichTab <- which(tab > 0, arr.ind = T)
-      
+
       # loop over the configurations
       for (rowNum in seq.int(nrow(whichTab))){
         config <- whichTab[rowNum, ]
-        
+
         # get conditional probability table for this configuraion
         L <- as.list(c(0, config))
         L[1] <- T
         probs <- do.call("[", c(list(ptable), L))
-        
+
         # for the rows that are in this configuration, sample
         whichRows <- apply(local, 1, function(x) all(x == config))
-        
+
         dat[whichRows, i] <- creator(numberOfLevels,
                                         size    = sum(whichRows),
                                         replace = T,
                                         prob    = probs)
       }
-      
+
       # old slow version
       # would be better to loop over the distinct values, rather than rows
       #for (j in 1:nsim){
@@ -244,7 +244,6 @@ simulate.bn <- function(object, nsim, seed, ptables, expectation = F, ...){
       #}
     }
   }
-  
   data.frame(lapply(dat, as.factor))
 }
 
@@ -262,24 +261,24 @@ marginal.probs <- function(bn, ptables, N){
     class(ptables) == "list",
     all(sapply(ptables, class) == "table")
   )
-  
+
   nNodes <- length(bn)
   numberOfParents <- sapply(bn, length)
-  
+
   # topologically sort
   nodeOrder <- topologicallyOrder(bn)
-  
+
   numberOfLevelsAll <- lapply(seq_len(nNodes), function(i){
     dim(ptables[[i]])[1]
   })
-  
+
   # loop through the nodes in order
   for (i in nodeOrder){
     ptable <- ptables[[i]]
-    
+
     # first dimension is the values
     numberOfLevels <- numberOfLevelsAll[[i]]
-    
+
     if (numberOfParents[i] == 0){
       dat[, i] <- sample.int(numberOfLevels,
                              size    = N,
@@ -288,29 +287,29 @@ marginal.probs <- function(bn, ptables, N){
     }
     else {
       parents <- bn[[i]]
-      
+
       local <- dat[, parents, drop = F]
       local <- data.frame(lapply(seq_len(ncol(local)), function(i){
         ls <- seq_len(numberOfLevelsAll[[parents[i]]])
         factor(local[, i], levels = ls)
       }))
-      
+
       # find all the configurations of the parents that exist
       tab <- table(local)
       whichTab <- which(tab > 0, arr.ind = T)
-      
+
       # loop over the configurations
       for (rowNum in seq.int(nrow(whichTab))){
         config <- whichTab[rowNum, ]
-        
+
         # get conditional probability table for this configuraion
         L <- as.list(c(0, config))
         L[1] <- T
         probs <- do.call("[", c(list(ptable), L))
-        
+
         # for the rows that are in this configuration, sample
         whichRows <- apply(local, 1, function(x) all(x == config))
-        
+
         dat[whichRows, i] <- sample.int(numberOfLevels,
                                         size    = sum(whichRows),
                                         replace = T,
